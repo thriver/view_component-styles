88c5fc2b417b9acf8a36f311ff122f1a384854f1
ryantaylor <2320507+ryantaylor@users.noreply.github.com>
2022-06-09 15:56:17 -0400

    Fix constant loading.

--- lib/view_component/styles.rb
+++ lib/view_component/styles.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'view_component/styles/component'
-require 'view_component/styles/controller'
-require 'view_component/styles/helper'
 require 'view_component/styles/version'
 
 module ViewComponent
   module Styles
+    autoload :Helper, 'view_component/styles/helper'
+    autoload :Controller, 'view_component/styles/controller'
+    autoload :Component, 'view_component/styles/component'
   end
 end
